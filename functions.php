978222ada9549c4693b0c63fab70227a719c6997
Guillaume Bouly <gbouly1@gmail.com>
2023-02-27 19:18:46 +0100

    Modifiction de certains détails de background et de couleur dans le menu, et création des boutons de quantités

--- functions.php
+++ functions.php
@@ -7,7 +7,9 @@ function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
-function montheme_theme_support(){
+function planty_theme_support(){
     add_theme_support('custom-logo'); // Logo Custom
     add_theme_support('title-tag'); // Titre du site
     }
+
+add_action('after_setup_theme', 'planty_theme_support');

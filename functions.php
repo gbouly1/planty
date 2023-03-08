e90fdaee69259f4ad4346c3e585035133e013553
Guillaume Bouly <gbouly1@gmail.com>
2023-03-08 10:12:58 +0100

    création de l'admin link

--- functions.php
+++ functions.php
@@ -12,6 +12,12 @@ function planty_theme_support(){
     add_theme_support('title-tag'); // Titre du site
     }
 
-   
+add_action('after_setup_theme', 'planty_theme_support'); 
 
-add_action('after_setup_theme', 'planty_theme_support');
+function add_search_form($items, $args) {
+    if( is_user_logged_in() && $args->theme_location == 'main-menu'){
+        $items .= '<li class="admin-nav"><a href="https://localhost/planty2/wp_admin" class="admin-link">Admin</a></li>';        
+    }
+return $items;
+}
+add_filter('wp_nav_menu_items','add_search_form', 10, 2); ?>
